8a5960932523cf7da45b00760e14f54807484383
Simon <shwaldemar@gmail.com>
2019-09-27 10:32:01 +0100

    basic app front end added & styled

---
+++ src/components/App/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from '../../public/favicon.ico';
+import './App.css';
+import BusinessList from '../BusinessList/BusinessList'
+import SearchBar from '../SearchBar/SearchBar'
+
+class App extends React.Component {
+  render(){
+    return (
+      <div class="App">
+        <h1>ravenous</h1>
+        <SearchBar />
+        <BusinessList />
+      </div>
+    )
+  }
+}
+
+
+export default App;
